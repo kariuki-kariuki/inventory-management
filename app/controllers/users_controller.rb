f27f4198c3d71212c9a3dd54404a7f1cb42e6e23
Martin <mkmartinoes@gmail.com>
2022-08-17 20:05:43 +0300

    rc: user authentication with cookies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,25 @@
 class UsersController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessed_entity
-  rescue_from ActiveRecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
-  skip_before_action :authorize
+  skip_before_action :authorize, only: :create
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else 
+      render json: { errors: user.errors.full_message }
+    end
+  end
 
   def show
     current_user = @current_user
     render json: current_user
   end
+
+
 
   private
 
