90c29a3103796644bc5d0525cc9806510203395d
Martin <mkmartinoes@gmail.com>
2022-08-20 23:09:46 +0300

    ft: create asset

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,2 +1,13 @@
 class AssetsController < ApplicationController
+
+  def create
+    new_asset = Asset.create!(asset_params)
+    render json: new_asset, status: :created
+  end
+
+  private
+
+  def asset_params
+    params.permit(:name, :amount)
+  end
 end
