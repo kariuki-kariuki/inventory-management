6a2de4f470a4dee64856982d1ffcc6715f2e6b61
Martin <mkmartinoes@gmail.com>
2022-08-21 09:46:18 +0300

    ft: admin && manager authorization

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,5 +1,7 @@
 class AssetsController < ApplicationController
 
+  skip_before_action: authorize_admin
+
   def create
     new_asset = Asset.create!(asset_params)
     render json: new_asset, status: :created
