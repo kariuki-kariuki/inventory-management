65301b5d8977b55998fc3e5c97c8f64941d74cd3
Martin <mkmartinoes@gmail.com>
2022-08-21 20:07:10 +0300

    ft: serialization

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :password_digest
+  attributes :id, :name, :email, :role
+  has_many :requests
+  has_many :asset, through: :requests
 end
