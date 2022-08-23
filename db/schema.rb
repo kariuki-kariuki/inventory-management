3661980d0553cbb72ed54f91e620baab9b402a3f
Martin <mkmartinoes@gmail.com>
2022-08-23 08:28:52 +0300

    fix: migration error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_21_165713) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_21_165938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,7 +41,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_21_165713) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role"
+    t.string "role"
+    t.string "password"
   end
 
 end
