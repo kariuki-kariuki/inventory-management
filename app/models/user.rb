4205c2727b25d27715c20fa1a1488be313823016
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-08-25 02:34:22 +0300

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
   has_many :requests
   has_many :assets, through: :requests
 end
