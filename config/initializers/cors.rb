267c66c057e619842fb02f1cf855ade8f73e5650
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-09-02 12:05:17 +0300

    fxbugs

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://127.0.0.1:4000"
+    origins "asset-management-gamma.vercel.app"
+    
 
     resource "*",
       headers: :any,
